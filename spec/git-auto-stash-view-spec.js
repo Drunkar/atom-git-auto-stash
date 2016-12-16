bbf08a0e65207d1ed553472bdae12651b1f2fbe9
Drunkar <drunkars.p@gmail.com>
2016-12-16 18:07:24 +0400

    Initial commit.

---
+++ spec/git-auto-stash-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GitAutoStashView from '../lib/git-auto-stash-view';
+
+describe('GitAutoStashView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
